4cd6095085bacc942dfe52530e1b39696e26711f
William High <whigh@netflix.com>
2015-10-16 16:47:40 -0700

    adding R directory and my first exploration script

---
+++ R/willhigh_college_enrollment_explore.R
@@ -0,0 +1,26 @@
+library(data.table)
+library(ggplot2)
+library(bit64)
+
+data.dir <- '/Users/whigh/data/datakind_kippla/dropbox'
+enrollment.file <- 'SALESFORCE-Enrollment.csv'
+college.file <- 'CollegeTierMapping.csv'
+student.id.file <- 'MasterIDTable.csv'
+
+enr.dat <- fread(paste(data.dir,enrollment.file,sep='/'))
+col.dat <- fread(paste(data.dir,college.file,sep='/'))
+sid.dat <- fread(paste(data.dir,student.id.file,sep='/'))
+
+name.v <- colnames(col.dat)
+name.v[1] <- 'SCHOOL__C'
+setnames(col.dat,name.v)
+
+enr.dat.subset <- subset(enr.dat,
+                         TYPE__C == 'College',
+                         select=c('ID','TYPE__C','SCHOOL__C'))
+
+tier.merge <- merge(enr.dat.subset,col.dat,by='SCHOOL__C')
+
+p <- ggplot(tier.merge,aes(COMPETITIVENESS_INDEX__C)) 
+p <- p + geom_histogram()
+print(p)
